9ce144f40ac7dc594d0cc09703e7cd7a8286176f
中山健太 <nakayamakenta@nakayamakentanoMacBook-Pro-2.local>
2022-10-15 12:36:00 +0900

    seventh-commit

--- pra_django/settings.py
+++ pra_django/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATIC_ROOT = str(BASE_DIR + "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
